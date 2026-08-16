abf3b02abece993c1eaf2e6d8b82e891cfdb5cc6
Rudra356 <rudranaskar66@gmail.com>
2026-08-17 01:25:40 +0530

    Practice method overloading in Java

---
+++ src/oops/methodoverloading/Calculator.java
@@ -0,0 +1,20 @@
+package oops.methodoverloading;
+
+public class Calculator {
+    public void add(int a, int b) {
+        System.out.println(a+b);
+    }
+    public void add(int a, int b,int c) {
+        System.out.println(a+b+c);
+    }
+    public void add(double a, double b) {
+        System.out.println(a+b);
+    }
+    public void add(int[] numbers){
+        var sum = 0;
+        for(int i=0;i<numbers.length;i++){
+            sum+=numbers[i];
+        }
+        System.out.println(sum);
+    }
+}
